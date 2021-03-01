2233e2dbb6e5351028a9fd0df1cbc2bfd53ce635
srikanthdata501 <77316360+srikanthdata501@users.noreply.github.com>
2021-03-01 14:27:02 +0530

    Update Churn.sql

--- Churn.sql
+++ Churn.sql
@@ -1,4 +1,4 @@
---select * from "ML_WORKSHOP"."PUBLIC"."CUSTOMER_CHURN" limit 100;
+/*--select * from "ML_WORKSHOP"."PUBLIC"."CUSTOMER_CHURN" limit 100; */
 /*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
